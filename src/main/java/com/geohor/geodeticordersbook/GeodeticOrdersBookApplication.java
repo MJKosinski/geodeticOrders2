f86bb24c2ece74a6b2c193f8a2cb0d3db06769ab
MJKosinski <kosinski.geodezja@gmail.com>
2019-05-10 09:23:03 +0200

    spring boot configuration

---
+++ src/main/java/com/geohor/geodeticordersbook/GeodeticOrdersBookApplication.java
@@ -0,0 +1,13 @@
+package com.geohor.geodeticordersbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeodeticOrdersBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeodeticOrdersBookApplication.class, args);
+    }
+
+}
